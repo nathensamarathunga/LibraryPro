f5cfd0cc2337ebe8d0298502e7e0e2d41df402d6
Nathen Samarathunga <thisisnathen@outlook.com>
2025-04-19 17:12:49 +0530

    Added home page

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,41 @@ public class Main {
 
     public static Scanner input = new Scanner(System.in);
 
+    public static void manageBooks() {
+
+    }
+
+    public static void manageMembers() {
+
+    }
+
+    public static void issueBooks() {
+
+    }
+
+    public static void returnBooks() {
+
+    }
+
+    public static void viewReports() {
+
+    }
+
+    public static int homePage() {
+
+        String[] options = {"Manage Books", "Manage Members", "Issue Books", "Return Books", "View Reports", "Exit"};
+
+        for(int i = 0; i < 6; i++){
+
+            System.out.println(" " + (i+1) + ". " + options[i]);
+
+        }
+
+        System.out.print("Choose option: ");
+        return input.nextInt();
+
+    }
+
     public static void login() {
 
         boolean loginState = false; //Store login state
@@ -32,6 +67,28 @@ public class Main {
 
         login();
 
+        int option = homePage();
+
+        switch (option) {
+            case 1:
+                manageBooks();
+                break;
+            case 2:
+                manageMembers();
+                break;
+            case 3:
+                issueBooks();
+                break;
+            case 4:
+                returnBooks();
+                break;
+            case 5:
+                viewReports();
+                break;
+            case 6:
+                break;
+
+        }
     }
 
 }
